23aee0c4f0e180037c846a3f7d340257e16c18f6
Requestform <125638263+Requestform@users.noreply.github.com>
2023-02-19 18:46:04 +0900

    Add files via upload

--- chat.js
+++ chat.js
@@ -11,21 +11,40 @@ database.ref("messages").on("child_added", function(snapshot) {
 	document.getElementById("messagesDiv").appendChild(messageElement);
 });
 
-// メッセージを送信する
-document.getElementById("messageForm").addEventListener("submit", function(event) {
+// ページが読み込まれた時に、ローカルストレージに保存された名前があれば、名前入力欄を変更不可能にする
+window.addEventListener("load", function() {
+	var savedName = localStorage.getItem("name");
+	if (savedName) {
+	var nameInput = document.getElementById("nameInput");
+	nameInput.value = savedName;
+	nameInput.disabled = true;
+	}
+	});
+	
+	// メッセージを送信する
+	document.getElementById("messageForm").addEventListener("submit", function(event) {
 	event.preventDefault();
 	var nameInput = document.getElementById("nameInput");
 	var messageInput = document.getElementById("messageInput");
 	var name = nameInput.value;
 	var text = messageInput.value;
 	if (name === "" || text === "") {
-		alert("名前とメッセージを入力してください。");
+	alert("名前とメッセージを入力してください。");
 	} else {
-		database.ref("messages").push({
-			name: name,
-			text: text
-		});
-		nameInput.value = "";
-		messageInput.value = "";
+	// 名前がすでに保存されているかどうかを確認する
+	var savedName = localStorage.getItem("name");
+	if (savedName && savedName !== name) {
+	alert("名前を変更するには、ページをリロードしてください。");
+	nameInput.value = savedName; // 入力欄に保存された名前を表示する
+	nameInput.disabled = true; // 名前を変更できないようにする
+	} else {
+	database.ref("messages").push({
+	name: name,
+	text: text
+	});
+	localStorage.setItem("name", name); // 名前を保存する
+	nameInput.disabled = true; // 名前を変更できないようにする
+	messageInput.focus(); // メッセージ入力欄にフォーカスする
 	}
-});
+	}
+	});
