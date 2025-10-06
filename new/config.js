f641902bb7ca0c0ab7fb98747029d32b3f927696
Requestform <125638263+Requestform@users.noreply.github.com>
2025-10-06 22:00:55 +0900

    Add files via upload

---
+++ new/config.js
@@ -0,0 +1,4 @@
+window.REQUEST_CONFIG = {
+  BASE_URL: 'https://script.google.com/macros/s/AKfycbwUBSixWUyNnA8WgsddEookEjEGDUMSwBYK0ReIjHrZYqMW0JR7dovWYDeaRWPWsT2T-A/exec',
+  ADMIN_KEY: 'seitokai2023'
+};
