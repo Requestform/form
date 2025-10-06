f841238d2a827bec1ff0885ed274b95c23dde203
Requestform <125638263+Requestform@users.noreply.github.com>
2025-10-06 22:06:58 +0900

    Update config.js

--- new/config.js
+++ new/config.js
@@ -1,4 +1,5 @@
 window.REQUEST_CONFIG = {
-  BASE_URL: 'https://script.google.com/macros/s/AKfycbwUBSixWUyNnA8WgsddEookEjEGDUMSwBYK0ReIjHrZYqMW0JR7dovWYDeaRWPWsT2T-A/exec',
+  BASE_URL: 'https://script.google.com/macros/s/AKfycbybZBYynx2fkFgQjWsq4ybyAAp3zfVOVyYTi9bbzc05e4wSvYdbN9q4pae7hQ80EeT5Hg/exec',
   ADMIN_KEY: 'seitokai2023'
 };
+
