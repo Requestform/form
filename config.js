f081e2835391da946184d8d4070c0a6a10417cf5
Requestform <125638263+Requestform@users.noreply.github.com>
2025-10-16 19:31:26 +0900

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+// Configuration for Music Request System
+// GASデプロイ後、以下のURLを設定してください
+
+window.MUSIC_REQUEST_CONFIG = {
+  GAS_BASE_URL: 'https://script.google.com/macros/s/AKfycbxgIsYLsR0baCYVy_9YG-_XipCf_mP01nqyUvHgc-6qnxhX3-LNIWNYs1vWww96_xct/exec',
+  ADMIN_KEY: 'seitokai2023', // must match ADMIN_KEY in MusicRequest.gs
+  USER_PAGE_URL: './Request/index.html' // URL to user-facing music request page
+};
+// Example GAS URL: 'https://script.google.com/macros/s/AKfycbyXxxxxx/exec'
